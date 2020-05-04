5606272b27935a806497ed5ae05c60e8fd671d49
Carlos Guijarro Martín <carlosguijarromartin@outlook.com>
2020-05-04 23:16:49 +0200

    Export and Import

---
+++ JavaScript Moderno Udemy/Export - Import/js/main.js
@@ -0,0 +1,5 @@
+import { add, subtract } from './math_functions.js';
+
+
+console.log(add(5,7));
+console.log(subtract(5,7));
---
+++ JavaScript Moderno Udemy/Export - Import/js/math_functions.js
@@ -0,0 +1,16 @@
+// export const add = (x,y) => {
+//     return x + y;
+// }
+
+const add = (x, y) => {
+    return x + y;
+}
+
+const subtract = (x, y) => {
+    return x - y;
+}
+
+export {
+    add,
+    subtract
+}
